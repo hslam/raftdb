27c7642fc3ed1f03635872fc22b005723b6e3306
hslam <791874158@qq.com>
2020-07-18 18:13:27 +0800

    update rpc

--- node/node.go
+++ node/node.go
@@ -2,68 +2,70 @@ package node
 
 import (
 	"fmt"
+	"github.com/hslam/handler/proxy"
+	"github.com/hslam/handler/render"
+	"github.com/hslam/mux"
+	"github.com/hslam/raft"
+	"github.com/hslam/rpc"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"sync"
-	"github.com/hslam/raft"
-	"github.com/hslam/mux"
-	"github.com/hslam/handler/proxy"
-	"github.com/hslam/handler/render"
-	"github.com/hslam/rpc"
 	"net/url"
 	"strconv"
+	"sync"
 )
 
 const (
-	network = "tcp"
-	codec = "pb"
-	MaxConnsPerHost=8
-	MaxIdleConnsPerHost=0
+	network             = "tcp"
+	codec               = "pb"
+	MaxConnsPerHost     = 8
+	MaxIdleConnsPerHost = 0
 )
+
 var (
-	setCommandPool			*sync.Pool
+	setCommandPool *sync.Pool
 )
+
 func init() {
-	setCommandPool= &sync.Pool{
+	setCommandPool = &sync.Pool{
 		New: func() interface{} {
 			return &SetCommand{}
 		},
 	}
 }
-const LeaderPrefix  = "LEADER:"
+
+const LeaderPrefix = "LEADER:"
 
 type Node struct {
-	mu			sync.RWMutex
-	host		string
-	port		int
-	rpc_port	int
-	data_dir	string
-	mux			*mux.Mux
-	render 		*render.Render
-	raft_node 	*raft.Node
-	http_server	*http.Server
-	rpc_transport	*rpc.Transport
-	db			*DB
-
+	mu            sync.RWMutex
+	host          string
+	port          int
+	rpc_port      int
+	data_dir      string
+	mux           *mux.Mux
+	render        *render.Render
+	raft_node     *raft.Node
+	http_server   *http.Server
+	rpc_transport *rpc.Transport
+	db            *DB
 }
 
-func NewNode(data_dir string, host string, port ,rpc_port,raft_port int,peers []string,join string) *Node {
+func NewNode(data_dir string, host string, port, rpc_port, raft_port int, peers []string, join string) *Node {
 	n := &Node{
-		host:   	host,
-		port:   	port,
-		rpc_port:rpc_port,
-		data_dir:   data_dir,
-		db:     	newDB(),
-		mux: 		mux.New(),
-		render:		render.NewRender(),
+		host:     host,
+		port:     port,
+		rpc_port: rpc_port,
+		data_dir: data_dir,
+		db:       newDB(),
+		mux:      mux.New(),
+		render:   render.NewRender(),
 	}
 	var err error
-	nodes :=make([]*raft.NodeInfo,len(peers))
-	for i,v:=range peers{
-		nodes[i]=&raft.NodeInfo{Address:v,Data:nil}
+	nodes := make([]*raft.NodeInfo, len(peers))
+	for i, v := range peers {
+		nodes[i] = &raft.NodeInfo{Address: v, Data: nil}
 	}
-	n.raft_node, err = raft.NewNode(host,raft_port,n.data_dir,n.db,false,nodes)
+	n.raft_node, err = raft.NewNode(host, raft_port, n.data_dir, n.db, false, nodes)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -71,12 +73,12 @@ func NewNode(data_dir string, host string, port ,rpc_port,raft_port int,peers []
 	n.raft_node.RegisterCommand(&SetCommand{})
 	n.raft_node.SetSnapshot(&Snapshot{})
 	n.raft_node.SetSyncTypes([]*raft.SyncType{
-		{86400,1},
-		{14400,1000},
-		{3600,10000},
-		{1800,50000},
-		{900,200000},
-		{60,5000000},
+		{86400, 1},
+		{14400, 1000},
+		{3600, 10000},
+		{1800, 50000},
+		{900, 200000},
+		{60, 5000000},
 	})
 	n.raft_node.SetCodec(&raft.ProtoCodec{})
 	n.raft_node.GzipSnapshot()
@@ -94,69 +96,65 @@ func NewNode(data_dir string, host string, port ,rpc_port,raft_port int,peers []
 		m.HandleFunc("/nodes", n.nodesHandler).All()
 	})
 	n.mux.HandleFunc("/db/:key", n.leaderHandle(n.getHandler)).GET()
-	n.mux.HandleFunc("/db/:key",n.leaderHandle(n.setHandler)).POST()
+	n.mux.HandleFunc("/db/:key", n.leaderHandle(n.setHandler)).POST()
 	return n
 }
-
 
 func (n *Node) ListenAndServe() error {
 	n.raft_node.Start()
 	log.Println("HTTP listening at:", n.uri())
-	log.Println("RPC listening at:", fmt.Sprintf("%s:%d", n.host,n.rpc_port))
-	service:=new(Service)
-	service.node=n
-	server:= rpc.NewServer()
-	server.RegisterName("S",service)
-	//server.EnableAsyncHandleWithSize(1024*256)
-	server.EnableMultiplexingWithSize(1024*256)
-	server.SetBatch(true)
+	log.Println("RPC listening at:", fmt.Sprintf("%s:%d", n.host, n.rpc_port))
+	service := new(Service)
+	service.node = n
+	server := rpc.NewServer()
+	server.RegisterName("S", service)
+	server.SetSize(1024 * 256)
+	server.SetBatching(true)
 	rpc.SetLogLevel(99)
-	if n.rpc_transport==nil{
-		n.InitRPCProxy(MaxConnsPerHost,MaxIdleConnsPerHost)
+	if n.rpc_transport == nil {
+		n.InitRPCProxy(MaxConnsPerHost, MaxIdleConnsPerHost)
 	}
 
 	go server.ListenAndServe("tcp", fmt.Sprintf(":%d", n.rpc_port))
 	return n.http_server.ListenAndServe()
 }
-func (n *Node) InitRPCProxy(MaxConnsPerHost int,MaxIdleConnsPerHost int){
-	opts:=rpc.DefaultOptions()
+func (n *Node) InitRPCProxy(MaxConnsPerHost int, MaxIdleConnsPerHost int) {
+	opts := rpc.DefaultOptions()
 	opts.SetRetry(false)
 	opts.SetCompressType("gzip")
 	opts.SetMultiplexing(true)
-	opts.SetBatch(true)
-	opts.SetBatchAsync(true)
-	n.rpc_transport=rpc.NewTransport(MaxConnsPerHost,MaxIdleConnsPerHost,network,codec,opts)
+	opts.SetBatching(true)
+	n.rpc_transport = rpc.NewTransport(MaxConnsPerHost, MaxIdleConnsPerHost, network, codec, opts)
 }
 func (n *Node) uri() string {
-	return fmt.Sprintf("http://%s:%d",  n.host, n.port)
+	return fmt.Sprintf("http://%s:%d", n.host, n.port)
 }
 
-func (n *Node) leaderHandle(hander http.HandlerFunc) http.HandlerFunc{
+func (n *Node) leaderHandle(hander http.HandlerFunc) http.HandlerFunc {
 	defer func() {
 		if err := recover(); err != nil {
 		}
 	}()
 	return func(w http.ResponseWriter, r *http.Request) {
-		if n.raft_node.IsLeader(){
-			hander(w,r)
-		}else {
-			leader:=n.raft_node.Leader()
-			if leader!=""{
+		if n.raft_node.IsLeader() {
+			hander(w, r)
+		} else {
+			leader := n.raft_node.Leader()
+			if leader != "" {
 				leader_url, err := url.Parse("http://" + leader)
-				if err!=nil{
+				if err != nil {
 					panic(err)
 				}
-				port,err:=strconv.Atoi(leader_url.Port())
-				if err!=nil{
+				port, err := strconv.Atoi(leader_url.Port())
+				if err != nil {
 					panic(err)
 				}
-				leader_url.Host=leader_url.Hostname() + ":" + strconv.Itoa(port-2000)
-				proxy.Proxy(w,r,leader_url.String()+r.URL.Path)
+				leader_url.Host = leader_url.Hostname() + ":" + strconv.Itoa(port-2000)
+				proxy.Proxy(w, r, leader_url.String()+r.URL.Path)
 			}
 		}
 	}
 }
-
 
 func (n *Node) setHandler(w http.ResponseWriter, req *http.Request) {
 	defer func() {
@@ -170,13 +168,13 @@ func (n *Node) setHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	value := string(b)
-	setCommand:=newSetCommand(params["key"], value)
+	setCommand := newSetCommand(params["key"], value)
 	_, err = n.raft_node.Do(setCommand)
 	setCommandPool.Put(setCommand)
 	if err != nil {
-		if err==raft.ErrNotLeader{
-			leader:=n.raft_node.Leader()
-			if leader!=""{
+		if err == raft.ErrNotLeader {
+			leader := n.raft_node.Leader()
+			if leader != "" {
 				http.Error(w, LeaderPrefix+leader, http.StatusBadRequest)
 				return
 			}
@@ -190,7 +188,7 @@ func (n *Node) getHandler(w http.ResponseWriter, req *http.Request) {
 		}
 	}()
 	params := n.mux.Params(req)
-	if ok:=n.raft_node.ReadIndex();ok{
+	if ok := n.raft_node.ReadIndex(); ok {
 		value := n.db.Get(params["key"])
 		w.Write([]byte(value))
 	}
@@ -200,13 +198,13 @@ func (n *Node) statusHandler(w http.ResponseWriter, req *http.Request) {
 		if err := recover(); err != nil {
 		}
 	}()
-	status:=&Status{
-		IsLeader:n.raft_node.IsLeader(),
-		Leader:n.raft_node.Leader(),
-		Node:n.raft_node.Address(),
-		Peers:n.raft_node.Peers(),
+	status := &Status{
+		IsLeader: n.raft_node.IsLeader(),
+		Leader:   n.raft_node.Leader(),
+		Node:     n.raft_node.Address(),
+		Peers:    n.raft_node.Peers(),
 	}
-	n.render.JSON(w,req,status,http.StatusOK)
+	n.render.JSON(w, req, status, http.StatusOK)
 }
 func (n *Node) leaderHandler(w http.ResponseWriter, req *http.Request) {
 	defer func() {
@@ -220,14 +218,14 @@ func (n *Node) readyHandler(w http.ResponseWriter, req *http.Request) {
 		if err := recover(); err != nil {
 		}
 	}()
-	n.render.JSON(w,req,n.raft_node.Ready(),http.StatusOK)
+	n.render.JSON(w, req, n.raft_node.Ready(), http.StatusOK)
 }
 func (n *Node) isLeaderHandler(w http.ResponseWriter, req *http.Request) {
 	defer func() {
 		if err := recover(); err != nil {
 		}
 	}()
-	n.render.JSON(w,req,n.raft_node.IsLeader(),http.StatusOK)
+	n.render.JSON(w, req, n.raft_node.IsLeader(), http.StatusOK)
 }
 func (n *Node) addressHandler(w http.ResponseWriter, req *http.Request) {
 	defer func() {
@@ -241,14 +239,14 @@ func (n *Node) peersHandler(w http.ResponseWriter, req *http.Request) {
 		if err := recover(); err != nil {
 		}
 	}()
-	n.render.JSON(w,req,n.raft_node.Peers(),http.StatusOK)
+	n.render.JSON(w, req, n.raft_node.Peers(), http.StatusOK)
 }
 func (n *Node) nodesHandler(w http.ResponseWriter, req *http.Request) {
 	defer func() {
 		if err := recover(); err != nil {
 		}
 	}()
-	nodes:=n.raft_node.Peers()
-	nodes=append(nodes,n.raft_node.Address())
-	n.render.JSON(w,req,nodes,http.StatusOK)
+	nodes := n.raft_node.Peers()
+	nodes = append(nodes, n.raft_node.Address())
+	n.render.JSON(w, req, nodes, http.StatusOK)
 }
--- node/service.go
+++ node/service.go
@@ -6,89 +6,87 @@ import (
 	"strconv"
 )
 
-
-
 type Service struct {
 	node *Node
 }
 
-func (s *Service)Set(req *Request, res *Response) error {
-	if s.node.raft_node.IsLeader(){
-		setCommand:=newSetCommand(req.Key, req.Value)
+func (s *Service) Set(req *Request, res *Response) error {
+	if s.node.raft_node.IsLeader() {
+		setCommand := newSetCommand(req.Key, req.Value)
 		_, err := s.node.raft_node.Do(setCommand)
 		setCommandPool.Put(setCommand)
-		if err==nil{
-			res.Ok=true
+		if err == nil {
+			res.Ok = true
 			return nil
 		}
 		return err
-	}else {
-		leader:=s.node.raft_node.Leader()
-		if leader!=""{
+	} else {
+		leader := s.node.raft_node.Leader()
+		if leader != "" {
 			leader_url, err := url.Parse("http://" + leader)
-			if err!=nil{
+			if err != nil {
 				panic(err)
 			}
-			port,err:=strconv.Atoi(leader_url.Port())
-			if err!=nil{
+			port, err := strconv.Atoi(leader_url.Port())
+			if err != nil {
 				panic(err)
 			}
-			leader_host:=leader_url.Hostname() + ":" + strconv.Itoa(port-1000)
-			return s.node.rpc_transport.Call("S.Set",req,res,leader_host)
+			leader_host := leader_url.Hostname() + ":" + strconv.Itoa(port-1000)
+			return s.node.rpc_transport.Call(leader_host, "S.Set", req, res)
 		}
 		return raft.ErrNotLeader
 	}
 }
 
-func (s *Service)Get(req *Request, res *Response) error {
-	if s.node.raft_node.IsLeader(){
-		if ok:=s.node.raft_node.Lease();ok{
+func (s *Service) Get(req *Request, res *Response) error {
+	if s.node.raft_node.IsLeader() {
+		if ok := s.node.raft_node.Lease(); ok {
 			value := s.node.db.Get(req.Key)
-			res.Result=[]byte(value)
-			res.Ok=true
+			res.Result = []byte(value)
+			res.Ok = true
 			return nil
 		}
 		return nil
-	}else {
-		leader:=s.node.raft_node.Leader()
-		if leader!=""{
+	} else {
+		leader := s.node.raft_node.Leader()
+		if leader != "" {
 			leader_url, err := url.Parse("http://" + leader)
-			if err!=nil{
+			if err != nil {
 				panic(err)
 			}
-			port,err:=strconv.Atoi(leader_url.Port())
-			if err!=nil{
+			port, err := strconv.Atoi(leader_url.Port())
+			if err != nil {
 				panic(err)
 			}
-			leader_host:=leader_url.Hostname() + ":" + strconv.Itoa(port-1000)
-			return s.node.rpc_transport.Call("S.Get",req,res,leader_host)
+			leader_host := leader_url.Hostname() + ":" + strconv.Itoa(port-1000)
+			return s.node.rpc_transport.Call(leader_host, "S.Get", req, res)
 		}
 		return raft.ErrNotLeader
 	}
 }
 
-func (s *Service)ReadIndexGet(req *Request, res *Response) error {
-	if s.node.raft_node.IsLeader(){
-		if ok:=s.node.raft_node.ReadIndex();ok{
+func (s *Service) ReadIndexGet(req *Request, res *Response) error {
+	if s.node.raft_node.IsLeader() {
+		if ok := s.node.raft_node.ReadIndex(); ok {
 			value := s.node.db.Get(req.Key)
-			res.Result=[]byte(value)
-			res.Ok=true
+			res.Result = []byte(value)
+			res.Ok = true
 			return nil
 		}
 		return nil
-	}else {
-		leader:=s.node.raft_node.Leader()
-		if leader!=""{
+	} else {
+		leader := s.node.raft_node.Leader()
+		if leader != "" {
 			leader_url, err := url.Parse("http://" + leader)
-			if err!=nil{
+			if err != nil {
 				panic(err)
 			}
-			port,err:=strconv.Atoi(leader_url.Port())
-			if err!=nil{
+			port, err := strconv.Atoi(leader_url.Port())
+			if err != nil {
 				panic(err)
 			}
-			leader_host:=leader_url.Hostname() + ":" + strconv.Itoa(port-1000)
-			return s.node.rpc_transport.Call("S.ReadIndexGet",req,res,leader_host)
+			leader_host := leader_url.Hostname() + ":" + strconv.Itoa(port-1000)
+			return s.node.rpc_transport.Call(leader_host, "S.ReadIndexGet", req, res)
 		}
 		return raft.ErrNotLeader
 	}
